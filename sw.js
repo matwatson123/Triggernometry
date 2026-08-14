354466f9d8e15126e4be618cca4c66c6ae686af3
matwatson123 <86637613+matwatson123@users.noreply.github.com>
2026-08-13 22:12:58 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const C='triggernometry-v2';const A=['./','./index.html','./game-data.json','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='triggernometry-v2-2';
+const A=['./','./index.html','./game-data.json','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
